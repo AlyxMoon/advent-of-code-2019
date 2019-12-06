e02942e57ecfd2e7532a866247367a640f57a8cc
Allister <allisterkmoon@gmail.com>
2019-12-05 21:13:42 -0800

    Day 3 solution part 2

---
+++ day3/day3_part2.js
@@ -0,0 +1,60 @@
+const { readFile } = require('fs')
+const { promisify } = require('util')
+
+const parseInputFile = async (fileLocation) => {
+  try {
+    const input = await promisify(readFile)(fileLocation, 'utf8')
+    return input.split('\n')
+      .map(line => line.trim().split(','))
+      .map(wire => wire.map(path => {
+        let [dx, dy] = [0, 0]
+        if (path[0] === 'U') dy = 1
+        if (path[0] === 'D') dy = -1
+        if (path[0] === 'L') dx = -1
+        if (path[0] === 'R') dx = 1
+        return [[dx, dy], parseInt(path.slice(1), 10)]
+      } ))
+  } catch (error) {
+    throw new Error(`Error when reading the input file: ${error.message}`)
+  }
+}
+
+const buildWireGrid = (wirePaths) => {
+  return wirePaths.reduce((grid, path, wireIndex) => {
+    let [wireX, wireY] = [0, 0]
+    let steps = 0
+    path.forEach(([[dx, dy], cellsTravelled]) => {
+      for (let i = 1; i <= cellsTravelled; i++) {
+        [wireX, wireY] = [wireX + dx, wireY + dy]
+        steps += 1
+
+        let index = `${wireX},${wireY}`
+        grid[index] = grid[index] || [0, 0]
+        grid[index][wireIndex] = steps
+      }
+    })
+
+    return grid
+  }, { '0,0': [0, 0] })
+}
+
+const findFewestCombinedWireSteps = (grid) => {
+  return Object.values(grid)
+    .filter(([wire1Steps, wire2Steps]) => wire1Steps && wire2Steps)
+    .reduce((smallestDistance, [wire1Steps, wire2Steps]) => Math.min(smallestDistance, wire1Steps + wire2Steps), Infinity)
+}
+
+const main = async () => {
+  try {
+    const inputFilePath = process.argv[2] || 'input.txt'
+    const input = await parseInputFile(inputFilePath)
+    const grid = buildWireGrid(input)
+    const output = findFewestCombinedWireSteps(grid)
+    console.log(output)
+  } catch (error) {
+    console.error('BIG OL ERROR: ', error.message)
+    process.exit()
+  }
+}
+
+main()
