df8322c01cd81ac877cc079c5edbdbdf44a1af1c
Allister <allisterkmoon@gmail.com>
2019-12-24 16:00:06 -0800

    Update unit tests and fix intcode computer bug - Added some tests for specific instructions on intcode computer - fixed bug where every parameter during relative mode would use the first parameter instead of the correct one. Fixed solution so day 9.1 works!

---
+++ test/unit/spec/IntcodeComputer/IntcodeProcess.spec.js
@@ -0,0 +1,69 @@
+const { IntcodeProcess } = require('@lib/programs/IntcodeProcess')
+
+describe('Intcode Process', () => {
+  describe('runtime instructions', () => {
+    it('Ends correctly on break instruction', () => {
+      const processArgs = {
+        program: [1101, 0, 0, 1, 99, 1101, 10, 10, 1]
+      }
+
+      const output = new IntcodeProcess(processArgs).run().getProgramState()
+      expect(output.program).toEqual([1101, 0, 0, 1, 99, 1101, 10, 10, 1])
+      expect(output.linesRead).toBe(2)
+      expect(output.index).toBe(4)
+      expect(output.finished).toBe(true)
+    })
+
+    it('ADD instruction works in each mode', () => {
+      const processArgs1 = { program: [1, 5, 6, 0, 99, 10, 20] }
+      const output1 = new IntcodeProcess(processArgs1).run().getProgramState()
+
+      const processArgs2 = { program: [1101, 5, 6, 0, 99, 10, 20] }
+      const output2 = new IntcodeProcess(processArgs2).run().getProgramState()
+
+      const processArgs3 = { program: [1001, 5, 6, 0, 99, 10, 20] }
+      const output3 = new IntcodeProcess(processArgs3).run().getProgramState()
+
+      const processArgs4 = { program: [109, 3, 201, 0, 8, 0, 99, 10, 20] }
+      const output4 = new IntcodeProcess(processArgs4).run().getProgramState()
+
+      const processArgs5 = { program: [109, 3, 2201, 4, 5, 0, 99, 10, 20] }
+      const output5 = new IntcodeProcess(processArgs5).run().getProgramState()
+
+      const processArgs6 = { program: [109, 3, 22201, 4, 5, -1, 99, 10, 20] }
+      const output6 = new IntcodeProcess(processArgs6).run().getProgramState()
+
+      expect(output1.program).toEqual([30, 5, 6, 0, 99, 10, 20])
+      expect(output2.program).toEqual([11, 5, 6, 0, 99, 10, 20])
+      expect(output3.program).toEqual([16, 5, 6, 0, 99, 10, 20])
+      expect(output4.program).toEqual([20, 3, 201, 0, 8, 0, 99, 10, 20])
+
+      expect(output5.program).toEqual([30, 3, 2201, 4, 5, 0, 99, 10, 20])
+      expect(output6.program).toEqual([109, 3, 30, 4, 5, -1, 99, 10, 20])
+    })
+
+    it('REL_OFFSET instruction works in each mode', () => {
+      const processArgs1 = { program: [9, 3, 99, 3] }
+      const output1 = new IntcodeProcess(processArgs1).run().getProgramState()
+
+      const processArgs2 = { program: [9, 3, 99, 0] }
+      const output2 = new IntcodeProcess(processArgs2).run().getProgramState()
+
+      const processArgs3 = { program: [9, 3, 99, -10] }
+      const output3 = new IntcodeProcess(processArgs3).run().getProgramState()
+
+      const processArgs4 = { program: [109, 3, 99, 6] }
+      const output4 = new IntcodeProcess(processArgs4).run().getProgramState()
+
+      const processArgs5 = { program: [209, 1, 209, 3, 99] }
+      const output5 = new IntcodeProcess(processArgs5).run().getProgramState()
+
+
+      expect(output1.relativeIndex).toBe(3)
+      expect(output2.relativeIndex).toBe(0)
+      expect(output3.relativeIndex).toBe(-10)
+      expect(output4.relativeIndex).toBe(3)
+      expect(output5.relativeIndex).toBe(100)
+    })
+  })
+})
--- test/unit/spec/IntcodeComputer/_intcode_helpers.spec.js
+++ test/unit/spec/IntcodeComputer/_intcode_helpers.spec.js
@@ -1,5 +1,7 @@
 
 const {
+  COMMANDS,
+  MODES,
   fillMemoryToAddress,
   parseInstruction,
 } = require('@lib/programs/_intcode_helpers.js')
@@ -22,6 +24,70 @@ describe('Intcode Computer helper methods', () => {
       expect(() => { return fillMemoryToAddress([], 10, null) }).toThrow()
       expect(() => { return fillMemoryToAddress([], 10, '10') }).toThrow()
       expect(() => { return fillMemoryToAddress([], 10, 1.423) }).toThrow()
+    })
+
+    it('correctly extends array when provided index is greater than length', () => {
+      const originalArray = [1, 2, 3, 4, 5]
+      fillMemoryToAddress(originalArray, 10)
+      expect(originalArray.length).toBe(11)
+    })
+
+    it('does not mutate array if index is not greater than length', () => {
+      const originalArray = [1, 2, 3, 4, 5]
+      fillMemoryToAddress(originalArray, 3)
+      expect(originalArray.length).toBe(5)
+
+      fillMemoryToAddress(originalArray, 4)
+      expect(originalArray.length).toBe(5)
+    })
+  })
+
+  describe('parseInstruction', () => {
+    it('throws error on invalid input', () => {
+      expect(() => { parseInstruction(0) }).toThrow()
+      expect(() => { parseInstruction(-10) }).toThrow()
+      expect(() => { parseInstruction('0') }).toThrow()
+      expect(() => { parseInstruction({}) }).toThrow()
+      expect(() => { parseInstruction([]) }).toThrow()
+      expect(() => { parseInstruction() }).toThrow()
+    })
+
+    it('throws error if opcode is not recognized', () => {
+      expect(() => { parseInstruction(80) }).toThrow()
+      expect(() => { parseInstruction(1080) }).toThrow()
+    })
+
+    it('throws error if mode is not recognized', () => {
+      expect(() => { parseInstruction(301) }).toThrow()
+      expect(() => { parseInstruction(5101) }).toThrow()
+      expect(() => { parseInstruction(41101) }).toThrow()
+    })
+
+    it('gets correct opcode from input', () => {
+      expect(parseInstruction(1).opcode).toBe(COMMANDS.ADD)
+      expect(parseInstruction(2).opcode).toBe(COMMANDS.MULT)
+      expect(parseInstruction(3).opcode).toBe(COMMANDS.READ)
+      expect(parseInstruction(4).opcode).toBe(COMMANDS.WRITE)
+      expect(parseInstruction(5).opcode).toBe(COMMANDS.JUMP_T)
+      expect(parseInstruction(6).opcode).toBe(COMMANDS.JUMP_F)
+      expect(parseInstruction(7).opcode).toBe(COMMANDS.LESS_THAN)
+      expect(parseInstruction(8).opcode).toBe(COMMANDS.EQUAL_TO)
+      expect(parseInstruction(9).opcode).toBe(COMMANDS.REL_OFFSET)
+      expect(parseInstruction(99).opcode).toBe(COMMANDS.BREAK)
+    })
+
+    it('gets correct modes from input', () => {
+      expect(parseInstruction(1).modes).toEqual([MODES.POSITION, MODES.POSITION, MODES.POSITION])
+      expect(parseInstruction(101).modes).toEqual([MODES.IMMEDIATE, MODES.POSITION, MODES.POSITION])
+      expect(parseInstruction(201).modes).toEqual([MODES.RELATIVE, MODES.POSITION, MODES.POSITION])
+
+      expect(parseInstruction(1001).modes).toEqual([MODES.POSITION, MODES.IMMEDIATE, MODES.POSITION])
+      expect(parseInstruction(2001).modes).toEqual([MODES.POSITION, MODES.RELATIVE, MODES.POSITION])
+      expect(parseInstruction(2101).modes).toEqual([MODES.IMMEDIATE, MODES.RELATIVE, MODES.POSITION])
+
+      expect(parseInstruction(11001).modes).toEqual([MODES.POSITION, MODES.IMMEDIATE, MODES.IMMEDIATE])
+      expect(parseInstruction(22001).modes).toEqual([MODES.POSITION, MODES.RELATIVE, MODES.RELATIVE])
+      expect(parseInstruction(22101).modes).toEqual([MODES.IMMEDIATE, MODES.RELATIVE, MODES.RELATIVE])
     })
   })
 })
